91d267c48b6251b815cdb87249e0fdc55b3e8362
Marcos Sepulcre <marcos2161999@gmail.com>
2019-10-18 18:26:17 +0200

    Archivos.java

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+// El programa "Hola, Mundo!" en Java
+public class HolaMundo {
+	public static void main (String[] args) {
+		System.out.println("Hola Mundo");
+		
+	}
+}
---
+++ Practica22.java
@@ -0,0 +1,7 @@
+// El programa "Hola, Mundo!" en Java
+public class Practica22 {
+	public static void main (String[] args) {
+		System.out.println("Práctica 22");
+		
+	}
+}
